ce7098c0fcf916b08206ecfc1643b6ffd48d60e9
Jefri Bulo <jefribulomakassar@gmail.com>
2026-06-08 10:03:33 +0800

    Update page.tsx

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,1 +1,8 @@
+// app/testing/page.tsx
+import dynamic from "next/dynamic";
 
+const TestingPage = dynamic(() => import("./TestingClient"), { ssr: false });
+
+export default function Page() {
+  return <TestingPage />;
+}
